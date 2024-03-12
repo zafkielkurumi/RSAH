14fdb7efb5806fbcaf5a52a60baaa58acda9904c
zafkielkurumi <563693600@qq.com>
2024-03-12 21:54:29 +0800

    攻略

--- modules/config.py
+++ modules/config.py
@@ -1,10 +1,10 @@
 # TODO 后续改成可修改配置
 # EMULATOR_PATH = 'C:/LDPlayer/LDPlayer9/dnplayer.exe'
-EMULATOR_PATH = 'C:\LDPlayer\LDPlayer9\dnplayer.exe index=3'
+EMULATOR_PATH = 'C:\LDPlayer\LDPlayer9\dnplayer.exe index=5'
 ADB_PATH = "./tools/adb/adb.exe"
 # APP_NAME = "com.megagame.crosscore/com.megagame.crosscore.extend.PhoneUtilActivity"
 APP_NAME = "com.hermes.goda/com.hermes.goda.MainActivity"
-EMULATOR_IP = "127.0.0.1:5561"
+EMULATOR_IP = "127.0.0.1:5565"
 SCREENSHOT_NAME = 'screenshot.png'
 
 
